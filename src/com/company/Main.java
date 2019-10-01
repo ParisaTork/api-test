2573858fb6823afa16a7f10420aa825fe1678ff5
ParisaTork <47482049+ParisaTork@users.noreply.github.com>
2019-10-01 21:43:58 +0100

    Method 2: java.net.http.HttpClient

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,54 +1,18 @@
 package com.company;
-// Method 1: java.net.HttpURLConnection
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+// Method 2: java.net.http.HttpClient
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
 
 public class Main {
-
-    private static HttpURLConnection connection;
-
-    public static void main(String[] args) {
-        BufferedReader reader;
-        String line;
-        StringBuffer responseContent = new StringBuffer();
-        try {
-            URL url = new URL("https://jsonplaceholder.typicode.com/albums");
-            connection = (HttpURLConnection) url.openConnection();
-
-            // request setup
-            connection.setRequestMethod("GET");
-            connection.setConnectTimeout(5000);
-            connection.setReadTimeout(5000);
-
-            int status = connection.getResponseCode();
-            System.out.println(status);
-
-            if (status > 299) {
-                reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
-                while((line = reader.readLine()) != null) {
-                    responseContent.append(line);
-                }
-                reader.close();
-            } else {
-                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                while((line = reader.readLine()) != null) {
-                    responseContent.append(line);
-                }
-                reader.close();
-            }
-            System.out.println(responseContent.toString());
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            connection.disconnect();
-        }
-
-    }
+     public static void main(String[] args) {
+          HttpClient client = HttpClient.newHttpClient();
+          HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://jsonplaceholder.typicode.com/albums")).build();
+          client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                  .thenApply(HttpResponse::body)
+                  .thenAccept(System.out::println)
+                  .join();
+     }
 }
