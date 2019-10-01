3230e9816cabbbe1e68180c443d10e96230302cd
ParisaTork <47482049+ParisaTork@users.noreply.github.com>
2019-10-01 22:04:06 +0100

    Parse with Method 1: java.net.HttpURLConnection

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,69 @@
+package com.company;
+// Method 1: java.net.HttpURLConnection
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Main {
+
+     private static HttpURLConnection connection;
+
+     public static void main(String[] args) {
+          BufferedReader reader;
+          String line;
+          StringBuffer responseContent = new StringBuffer();
+          try {
+               URL url = new URL("https://jsonplaceholder.typicode.com/albums");
+               connection = (HttpURLConnection) url.openConnection();
+
+               // request setup
+               connection.setRequestMethod("GET");
+               connection.setConnectTimeout(5000);
+               connection.setReadTimeout(5000);
+
+               int status = connection.getResponseCode();
+               System.out.println(status);
+
+               if (status > 299) {
+                    reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+                    while((line = reader.readLine()) != null) {
+                         responseContent.append(line);
+                    }
+                    reader.close();
+               } else {
+                    reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    while((line = reader.readLine()) != null) {
+                         responseContent.append(line);
+                    }
+                    reader.close();
+               }
+               parse(responseContent.toString());
+
+          } catch (MalformedURLException e) {
+               e.printStackTrace();
+          } catch (IOException e) {
+               e.printStackTrace();
+          } finally {
+               connection.disconnect();
+          }
+
+     }
+
+     public static String parse(String responseBody) {
+          JSONArray albums = new JSONArray(responseBody);
+          for (int i = 0; i < albums.length(); i++) {
+               JSONObject album = albums.getJSONObject(i);
+               int id = album.getInt("id");
+               int userId = album.getInt("userId");
+               String title = album.getString("title");
+               System.out.println(id + " " + title + " " + userId);
+          }
+          return null;
+     }
+}
