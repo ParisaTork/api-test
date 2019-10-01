514fc394fee7c78607644a94731636d12a46005f
ParisaTork <47482049+ParisaTork@users.noreply.github.com>
2019-10-01 22:42:15 +0100

    Parse Method 2: java.net.http.HttpClient

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,70 +1,32 @@
 package com.company;
-// Method 1: java.net.HttpURLConnection
+// Method 2: java.net.http.HttpClient
 import org.json.JSONArray;
 import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
 
 public class Main {
+    public static void main(String[] args) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://jsonplaceholder.typicode.com/albums")).build();
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenApply(Main::parse)
+                .join();
+    }
 
-     private static HttpURLConnection connection;
-
-     public static void main(String[] args) {
-          BufferedReader reader;
-          String line;
-          StringBuffer responseContent = new StringBuffer();
-          try {
-               URL url = new URL("https://jsonplaceholder.typicode.com/albums");
-               connection = (HttpURLConnection) url.openConnection();
-
-               // request setup
-               connection.setRequestMethod("GET");
-               connection.setConnectTimeout(5000);
-               connection.setReadTimeout(5000);
-
-               int status = connection.getResponseCode();
-               System.out.println(status);
-
-               if (status > 299) {
-                    reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
-                    while((line = reader.readLine()) != null) {
-                         responseContent.append(line);
-                    }
-                    reader.close();
-               } else {
-                    reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                    while((line = reader.readLine()) != null) {
-                         responseContent.append(line);
-                    }
-                    reader.close();
-               }
-               parse(responseContent.toString());
-
-          } catch (MalformedURLException e) {
-               e.printStackTrace();
-          } catch (IOException e) {
-               e.printStackTrace();
-          } finally {
-               connection.disconnect();
-          }
-
-     }
-
-     public static String parse(String responseBody) {
-          JSONArray albums = new JSONArray(responseBody);
-          for (int i = 0; i < albums.length(); i++) {
-               JSONObject album = albums.getJSONObject(i);
-               int id = album.getInt("id");
-               int userId = album.getInt("userId");
-               String title = album.getString("title");
-               System.out.println(id + " " + title + " " + userId);
-          }
-          return null;
-     }
+    public static String parse(String responseBody) {
+        JSONArray albums = new JSONArray(responseBody);
+        for (int i = 0; i < albums.length(); i++) {
+            JSONObject album = albums.getJSONObject(i);
+            int id = album.getInt("id");
+            int userId = album.getInt("userId");
+            String title = album.getString("title");
+            System.out.println(id + " " + title + " " + userId);
+        }
+        return null;
+    }
 }
-
